6d8a8356d5d4d968b1a916b51672f45f3430d4e5
Dmytro Nikolaiev <vialaky@gmail.com>
2025-09-07 21:35:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -59,7 +59,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     modal.className = 'modal';
     modal.innerHTML = `
       <div class="modal-content">
-      <span class="modal-close"><svg viewBox="0 0 24 24" width="24" height="24"><path fill="#1a2a57" d="M18 6L6 18M6 6l12 12" stroke="#1a2a57" stroke-width="2" stroke-linecap="round"/></svg></span>      
+      <span class="modal-close"><svg viewBox="0 0 24 24" width="24" height="24"><path fill="#1a2a57" d="M18 6L6 18M6 6l12 12" stroke="#1a2a57" stroke-width="2" stroke-linecap="round"/></svg></span>
+        <img src="${imageSrc}" alt="${altText}" class="modal-image">
       </div>
     `;
     document.body.appendChild(modal);
